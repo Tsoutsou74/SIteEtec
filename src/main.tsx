779f57e3882b91f7709c2c38bfad3e317a91715b
tahiry <tahirydev29@gmail.com>
2026-07-09 13:14:45 +0300

    thr/frontend_fix: update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.css'
 import App from './App.tsx'
 import { I18nProvider } from './config/I18nProvider';
+import ErrorBoundary from './components/ErrorBoundary';
 
-createRoot(document.getElementById('root')).render(
-   <I18nProvider>
+createRoot(document.getElementById('root')!).render(
+  <I18nProvider>
     <StrictMode>
-      <App />
+      <ErrorBoundary>
+        <App />
+      </ErrorBoundary>
     </StrictMode>
   </I18nProvider>
 )
+
